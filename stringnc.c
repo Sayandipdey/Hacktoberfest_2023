95103d6ea23282e4d600da3b145d9ed43945ccb9
Sayandipdey <147439200+Sayandipdey@users.noreply.github.com>
2023-10-16 00:56:58 +0530

    Create stringnc.c

---
+++ stringnc.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() 
+{
+     printf("Enter number of letters in words ");
+      int n;    
+scanf("%d", n);            
+char input[n];
+printf("Enter a string: ");
+scanf("%s", input);
+
+    printf("You entered: %s\n", input);
+
+    return 0;
+}
